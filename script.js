8a3f03e7ebb1bb335629f33d36040e778ac4e4a4
brad hendrickse <bradh@box.org.za>
2022-03-07 17:02:13 +0200

    added ability to add and delete books. refactored some code to functions, etc..

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const newBookButton = document.getElementById('newBookButton');
+const newBookForm = document.getElementById('newBookForm');
 const newBookFormCancel = document.getElementById('newBookFormCancel');
 const newBookModal = document.getElementById('newBookModal');
 const libraryContainer = document.getElementById('libraryContainer');
@@ -25,39 +26,105 @@ let myLibrary = [
   },
 ];
 
-function Book() {
-  // the constructor...
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const pages = document.getElementById('pages');
+  const read = document.getElementById('read');
+
+  console.log(read.checked);
+  const newBook = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    read.checked
+  );
+
+  myLibrary.push(newBook);
+  closeNewBookModal();
+}
+
+function closeNewBookModal() {
+  newBookModal.classList.remove('open');
 }
 
 newBookButton.addEventListener('click', (event) => {
   event.preventDefault();
+
+  newBookForm.reset();
+
   newBookModal.classList.add('open');
 
   // close the modal if you click anywhere outside of the content
   window.onclick = function (event) {
     if (event.target === newBookFormCancel || event.target === newBookModal) {
-      newBookModal.classList.remove('open');
+      closeNewBookModal();
     }
   };
 });
 
-function renderLibraryHtml() {
-  libraryContainer.innerHTML = '';
-  myLibrary.forEach((book) => {
-    const bookHtml = `
-      <div class="bookCard">
-        <div class="bookTitle"><p>Title: ${book.title}</p></div>
-        <div class="bookAuthor"><p>Author: ${book.author}</p></div>
-        <div class="bookPages"><p>Pages: ${book.pages}</p></div>
-        <div class="bookRead"><p>Read: ${book.read}</p></div>
-      </div>
-      `;
-    libraryContainer.insertAdjacentHTML('afterbegin', bookHtml);
+newBookForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addBookToLibrary();
+  renderLibraryHTML();
+});
+
+function removeEventHandlers() {
+  myLibrary.forEach((book, index) => {
+    myLibrary[index].deleteButton = document.getElementById(`${index}`);
+    myLibrary[index].deleteButton.removeEventListener(
+      'click',
+      deleteBookFromLibrary
+    );
   });
 }
 
-renderLibraryHtml();
+const deleteBookFromLibrary = (event) => {
+  const index = event.target.id;
+  if (
+    confirm(
+      `Are you sure you want to delete "${myLibrary[index].title}" from the library?`
+    )
+  ) {
+    myLibrary.splice(index, 1);
+    removeEventHandlers();
+    renderLibraryHTML();
+  }
+};
+
+function renderLibraryHTML() {
+  libraryContainer.innerHTML = '';
+  myLibrary.forEach((book, index) => {
+    const bookHtml = `
+      <div class="bookCard" id="${index}">
+        <div class="bookTitle"><strong>Title:</strong> ${book.title}</div>
+        <div class="bookAuthor"><strong>Author:</strong> ${book.author}</div>
+        <div class="bookPages"><strong>Pages:</strong> ${book.pages}</div>
+        <div class="bookRead"><strong>Read:</strong> ${book.read}</div>
+        <div class="bookDelete"><span class="material-icons-outlined md-36 delete-button" id="${index}">delete</span></div>
+      </div>
+      `;
+    libraryContainer.insertAdjacentHTML('afterbegin', bookHtml);
+
+    myLibrary[index].deleteButton = document.getElementById(`${index}`);
+    myLibrary[index].deleteButton.addEventListener(
+      'click',
+      deleteBookFromLibrary
+    );
+  });
+  if (myLibrary.length === 0) {
+    libraryContainer.insertAdjacentHTML('afterbegin', `no books here`);
+  }
+  console.log(myLibrary);
+}
+
+renderLibraryHTML();
